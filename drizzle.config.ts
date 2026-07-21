fbf03fb8604a76ec7dff70ece318b0a50b04304c
charlesagn <charles@guidance.tools>
2026-07-21 20:50:50 +0200

    Add Neon DB + Drizzle schema + pages

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_ztMXZ7SYEHq9@ep-calm-thunder-a268jk9x-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   },
 });
